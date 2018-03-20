eef0e055bbf270c5ad7cadd19adbba6ee841b210
Julian Vu <julianvu@outlook.com>
2018-03-20 09:12:24 -0700

    Created ClockHand starter

---
+++ src/ClockHand.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class ClockHand implements MoveableShape {
+    @Override
+    public void draw(Graphics2D g2) {
+
+    }
+
+    @Override
+    public void translate(int dx, int dy) {
+
+    }
+}
