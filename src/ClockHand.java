58891e62bd83e80024ce1a12412d6107399f2b95
Julian Vu <julianvu@outlook.com>
2018-03-24 20:35:57 -0700

    Revert "Fixed ClockHand"
    This reverts commit d20c6fd104ee91b9b755e8890655a82c9af3a5d1.

--- src/ClockHand.java
+++ src/ClockHand.java
@@ -1,30 +1,32 @@
 import java.awt.*;
 import java.awt.geom.Line2D;
 
-public class ClockHand implements MoveableShape {
+public class ClockHand implements MoveableShape, Stroke {
     // instance variables
-    private double x1, x2, y1, y2, length;
+    private int x1, x2, y1, y2, length;
 
 
-    public ClockHand(double x1, double y1, double length) {
+    public ClockHand(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
-        this.x2 = x1;
-        this.y2 = y1 - length;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
     @Override
     public void draw(Graphics2D g2) {
         Line2D.Double hand = new Line2D.Double(x1, y1, x2, y2);
-        Stroke stroke = new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1f);
-        g2.setStroke(stroke);
-        g2.setColor(Color.BLACK);
         g2.draw(hand);
     }
 
     @Override
     public void translate(int dx, int dy) {
-        this.x2 += dx;
-        this.y2 += dy;
+        x2 += dx;
+        y2 += dy;
+    }
+
+    @Override
+    public Shape createStrokedShape(Shape p) {
+        return null;
     }
 }
